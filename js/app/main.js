e5f6c5231742daa332281c68fabe47967f3ff985
Piotr Kulpa <p.kulpa@onet.eu>
2016-11-28 23:57:08 +0100

    object way

--- js/app/main.js
+++ js/app/main.js
@@ -1,48 +1,38 @@
 function getImg(){
-
-var allImages = '';
-var columns = '';
-var j = 0;
-var i = 0;
-var k = 1;
-var imgNumber = 10;
-var columnNumber = 4;
-var mosaicWidth = 100 / columnNumber;
-
-
-
-for(k=1; k<=columnNumber; k++){
-  columns = '<div id="photos' + k + '" class="mosaic"></div>';
-  $('.container').append(columns);
-
-}
-
-$('.mosaic').css('width', mosaicWidth + '%');
-$('.container').append('<hr>');
-var timer =  setInterval(function(){
-  myTimer();
-
-}, 100);
-
-function killInterval(){
-  clearInterval(timer);
-}
-
-function myTimer(){
-  i++;
-  j++;
-
-  if(j>columnNumber){
-    j=1;
-  }
-  if(i>imgNumber){
-    killInterval();
-    $('hr').fadeIn();
-  }else{
-  allImages = '<img src="./images/' + i + '.jpg">';
-  $('#photos' + j).append(allImages);
-  $('[src="./images/' + i + '.jpg"]').fadeIn(700);
-  }
-}
-
+  this.allImages = '';
+  this.columns= '';
+  this.j = 0;
+  this.i =0;
+  this.k = 1;
+  this.imgNumber = 20; //set number of images
+  this.columnNumber = 3; //set number of columns
+  this.mosaicWidth = function() {return 100 / this.columnNumber + '%'}; //set mosaic class width in %
+  this.insertColumns = function() {
+    for(this.k = 1; this.k <= this.columnNumber; this.k++){
+      this.columns = '<div id="photos' + this.k + '" class="mosaic"></div>';
+      $('.container').append(this.columns);
+      $('.mosaic').css('width', this.mosaicWidth());
+    }
+  };
+  this.myTimer = function() { // main function
+    this.j++;
+    this.i++;
+    if(this.j > this.columnNumber){
+      this.j=1;
+    }
+    if(this.i > this.imgNumber){
+      this.killInterval();
+      $('hr').fadeIn();
+    }else{
+    this.allImages = '<img src="./images/' + this.i + '.jpg">';
+    $('#photos' + this.j).append(this.allImages);
+    $('[src="./images/' + this.i + '.jpg"]').fadeIn(500);
+    }
+  };
+  this.killInterval = function() { //clears interval
+    clearInterval(timer);
+  };
+  this.insertColumns();
+  var timer =  setInterval(this.myTimer, 100);
+  $('.container').append('<hr>');
 }
